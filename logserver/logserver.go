c43567d9d7490586f9e9b00064cacdd3d7d46969
Brian Bright <bbright123@yahoo.com>
2018-04-07 16:35:09 -0700

    changed some default settings

--- logserver/logserver.go
+++ logserver/logserver.go
@@ -6,12 +6,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-//	"net/url"
 	"os"
-//	"strings"
 )
 
-var logdir = flag.String("d", "/home/bbright/", "directory containing app log files")
+var logdir = flag.String("d", "./", "directory containing app log files")
 var port = flag.String("p", ":8888", "log server port")
 var addr = flag.String("addr", "localhost", "ip address of server")
 var filename = flag.String("f", "mainapp.log", "name of main app log file")
@@ -19,19 +17,22 @@ var filename = flag.String("f", "mainapp.log", "name of main app log file")
 func main() {
 	flag.Parse()
 
-
-
-	http.HandleFunc("/readLog", readLog)
+	http.HandleFunc("/read", readLog)
 	log.Fatal(http.ListenAndServe(*addr+*port, nil))
 }
 
+// reads the entire log file and prints it to the output
 func readLog(w http.ResponseWriter, r *http.Request) {
 	logfile, err := os.Open(*logdir + *filename)
-	if err != nil {
+	if os.IsNotExist(err) {
+		fmt.Println(w, "The log file does not exist")
+		return
+	} else if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	defer logfile.Close()
+	
 	scanner := bufio.NewScanner(logfile)
 	for scanner.Scan() {
 		fmt.Fprintln(w, scanner.Text())
@@ -40,12 +41,3 @@ func readLog(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(os.Stderr, "reading from logfile:", err)
 	}
 }
-
-//func search(w http.ResponseWriter, r *http.Request) {
-
-//}
-
-// opens a log file and returns a map of dates to
-//func parseLog() {
-
-//}
