70b9472acc538578969a1597f050e399966dd3d7
Brian Bright <bbright123@yahoo.com>
2018-04-07 22:33:47 -0700

    changed from flags to parsing a conf.json file

--- logserver/logserver.go
+++ logserver/logserver.go
@@ -1,48 +1,108 @@
 // When run, sits on a specified port and responds with
 // a specified log file.
 //
-// Usage: logserver -addr "ip address for server" -p ":PORT" -d "/directory/of/log/" -f "logfile_name" 
+// Usage: logserver "/path/to/conf/file/conf.json"
 
 package main
 
 import (
+	"strings"
 	"bufio"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
 )
 
-var logdir = flag.String("d", "./", "directory containing app log files")
-var port = flag.String("p", ":8888", "log server port")
-var addr = flag.String("addr", "localhost", "ip address of server")
-var filename = flag.String("f", "mainapp.log", "name of main app log file")
-
-func main() {
-	flag.Parse()
-
-	http.HandleFunc("/read", readLog)
-	log.Fatal(http.ListenAndServe(*addr+*port, nil))
+type Conf struct {
+	Dir     string
+	Address string
+	Port    string
+	Logfile string
 }
 
-// reads the entire log file and prints it to the socket
-func readLog(w http.ResponseWriter, r *http.Request) {
-	logfile, err := os.Open(*logdir + *filename)
-	if os.IsNotExist(err) {
-		fmt.Println(w, "The log file does not exist")
-		return
-	} else if err != nil {
-		log.Fatal(err)
-	}
+type LogEntry struct {
+	Logtime string
+	Message string
+}
+
+// default configuration settings
+var conf = Conf{
+	Dir:      "./",
+	Address:  "localhost",
+	Port:     ":8888",
+	Logfile: "mainapp.log",
+}
+
+const (
+	MaxLogEntries = 200
+)
+
+var logfile *os.File
+
+func main() {
 	
+	if len(os.Args) > 1 {
+		confFile, err := os.Open(os.Args[1])
+		if err == nil && !os.IsNotExist(err) {
+			defer confFile.Close()
+
+			err = json.NewDecoder(confFile).Decode(&conf)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			
+		} else if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Printf("Conf file in use: ")
+	fmt.Print(conf)
+	logfile, err2 := os.Open(conf.Dir + conf.Logfile)
+	if os.IsNotExist(err2) {
+		fmt.Fprintln(os.Stderr, "The log file does not exist")
+		return
+	} else if err2 != nil {
+		log.Fatal(err2)
+	}
 	defer logfile.Close()
 	
-	scanner := bufio.NewScanner(logfile)
-	for scanner.Scan() {
-		fmt.Fprintln(w, scanner.Text())
+	http.HandleFunc("/read", readLog)
+	err3 := http.ListenAndServe(conf.Address + conf.Port, nil)
+	fmt.Fprintf(os.Stderr, "Possible conf file error: %v\n", conf)
+	log.Fatal(err3)
+}
+
+// reads the entire log file and prints it to the socket buffer
+func readLog(w http.ResponseWriter, r *http.Request) {
+	logfile, err := os.Open(conf.Dir + conf.Logfile)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer logfile.Close()
+
+	// not sure if the last arg is taking bytes or slots for string pointers.
+	// its filling an array of structs that are themselves arrays of 2 strings,
+	// so it SHOULD be slots for string pointers.  will have to make sure later.
+	var logs = make([]LogEntry, 0, MaxLogEntries) 
+	scanner := bufio.NewScanner(logfile)
+
+	// the log files are in a predetermined format - let's grab it all
+	// and turn each entry into a struct. 
+	for scanner.Scan() {
+		temp := strings.Split(scanner.Text(), ", ")
+		le := LogEntry{Logtime:temp[0], Message:strings.Join(temp[1:], ", ")}
+		logs = append(logs, le)
+	}
+
+	fmt.Fprintf(w, " %-9.9s\tMessage\n", "Date")
+	for _, le := range logs {
+		fmt.Fprintf(w, "#%-9.9s\t%s\n", le.Logtime, le.Message) 
+	}
+	
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading from logfile:", err)
+		fmt.Fprintln(os.Stderr, "error reading from logfile:", err)
 	}
 }
